b75f6d185b5fabaf885e93b9d2724ea1be6f0d4e
xiewei18 <xiewei.link@outlook.com>
2023-02-05 22:03:27 +0800

    make panel more friendly for use.

--- Action.php
+++ Action.php
@@ -119,5 +119,4 @@ function addFilesToZip($path,$omitpath,$zip){
   @closedir($path);
 }
 
-
 ?>
--- panel.php
+++ panel.php
@@ -11,8 +11,12 @@ include 'menu.php';
     </div>
     <div class="row typecho-page-main" role="form">
       <div id="dbmanager-plugin" class="col-mb-12 col-tb-8 col-tb-offset-2">
-        <p>在您点击下面的按钮后，Typecho 会创建一个 Zip 压缩文件 (通常位于 /tmp/Export2Hugo/ 目录内)，包含所有的文章和页面，供您保存到计算机中。</p>
+        <p>在您点击下面的按钮后，Typecho 会创建一个 名为 hugo-yyyymmdd.zip 的 Zip 压缩文件 (通常位于 /tmp/Export2Hugo/ 目录内)，包含所有的文章和页面，供您保存到计算机中。</p>
+        <p>导出的文件是 Markdown 格式，可以直接导入到 Hugo 中。</p>
+        <p>压缩包中，文件名将以 yyyy-mm-dd postname.md 的格式存储。</p>
+        <p>点击后请稍候，压缩包将被自动下载到您的本地计算机。</p>
         <p>使用过程中如果有问题，请到 <a href="https://github.com/xiewei18/typecho2hugo/issues">Github</a> 提出。</p>
+        <p>Version: 1.0.0, last modified by <a href="https://www.xiewei.link">Wei</a></p>
         <form action="<?php $options->index('/action/export2hugo?export'); ?>" method="post">
           <ul class="typecho-option typecho-option-submit" id="typecho-option-item-submit-3">
             <li>
