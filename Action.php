d660a906cf2dd26da285afb50ecabab417c46978
xiewei18 <xiewei.link@outlook.com>
2023-02-05 21:50:47 +0800

    omit duplicate paths in the output zip file.

--- Action.php
+++ Action.php
@@ -72,7 +72,7 @@ TMP;
     $zip=new ZipArchive();
     // open($outputFile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
     if($zip->open($outputFile, ZipArchive::CREATE | ZipArchive::OVERWRITE)=== TRUE){
-        addFilesToZip($contentDir, $zip); //调用方法，对要打包的根目录进行操作，并将ZipArchive的对象传递给方法
+        addFilesToZip($contentDir,$contentDir, $zip); //调用方法，对要打包的根目录进行操作，并将ZipArchive的对象传递给方法
         $zip->close(); //关闭处理的zip文件
     }
     
@@ -105,14 +105,14 @@ function delTree($dir) {
    return rmdir($dir);
  }
 
-function addFilesToZip($path,$zip){
+function addFilesToZip($path,$omitpath,$zip){
   $handler=opendir($path); //打开当前文件夹由$path指定。
   while(($filename=readdir($handler))!==false){
       if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..'，不要对他们进行操作
-          if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
-              addFilesToZip($path."/".$filename, $zip);
+          if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归  
+            addFilesToZip($path."/".$filename,$omitpath,$zip);
           }else{ //将文件加入zip对象
-              $zip->addFile($path."/".$filename,"hugo/".$filename);
+              $zip->addFile($path."/".$filename,str_replace($omitpath,"hugo",$path."/").$filename);
           }
       }
   }
